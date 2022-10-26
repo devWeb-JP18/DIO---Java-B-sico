7ede7549537ea3bd9c36a660bb210bf05bad2000
devWeb-JP18 <jpkung16@outlook.com>
2022-10-26 18:23:54 -0300

    Introdução a anatomia das classes

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        //Definindo variáveis constantes: usar somente letras maiúsculas.
+        final String BR = "Brasil";
+        int ESTADOS_BRASILEIROS = 27;
+        double PI = 3.14;
+ 
+    }
+    
+}
